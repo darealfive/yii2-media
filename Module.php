eeb6e9f78d4c9bdb5572bc8c84a5ea6bb28d5188
Sebastian Krein <sebastian@itstrategen.de>
2018-09-08 16:09:29 +0200

    Configure virtual property "imageBasePath" as a directory

--- Module.php
+++ Module.php
@@ -2,6 +2,45 @@
 
 namespace darealfive\media;
 
+use darealfive\base\behaviors\property\DirectoryBehavior;
+use yii\base\InvalidConfigException;
+
+/**
+ * Class Module
+ *
+ * @package darealfive\media
+ * @property \yii\base\Module $module
+ * @property string           $imageBasePath the configurable directory in which the images are located.
+ */
 class Module extends \darealfive\base\Module
 {
+    /**
+     * Configures virtual property $imageBasePath as a directory to store images
+     *
+     * @return array
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            'imageBasePathDirectory' => [
+                'class'        => DirectoryBehavior::class,
+                'propertyName' => 'imageBasePath'
+            ]
+        ]);
+    }
+
+    /**
+     * Checks that this module is configured properly
+     *
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        echo $this->imageBasePath;
+        if (!is_dir($this->imageBasePath)) {
+
+            throw new InvalidConfigException(sprintf('Path "%s" is not a directory', $this->imageBasePath));
+        }
+    }
 }
--- controllers/Controller.php
+++ controllers/Controller.php
@@ -2,12 +2,14 @@
 
 namespace darealfive\media\controllers;
 
-use \darealfive\base\Controller as BaseController;
+use darealfive\base\Controller as BaseController;
+use darealfive\media\Module;
 
 /**
  * Class Controller
  *
  * @package darealfive\media\controllers
+ * @property Module $module
  */
 abstract class Controller extends BaseController
 {
