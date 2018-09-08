157688613f02f6ef744ba0abda4001e9ab74975c
Sebastian Krein <sebastian@itstrategen.de>
2018-09-08 16:20:53 +0200

    Remove behavior config as this can be achived in application configuration via "as ..."

--- Module.php
+++ Module.php
@@ -15,21 +15,6 @@ use yii\base\InvalidConfigException;
 class Module extends \darealfive\base\Module
 {
     /**
-     * Configures virtual property $imageBasePath as a directory to store images
-     *
-     * @return array
-     */
-    public function behaviors()
-    {
-        return array_merge(parent::behaviors(), [
-            'imageBasePathDirectory' => [
-                'class'        => DirectoryBehavior::class,
-                'propertyName' => 'imageBasePath'
-            ]
-        ]);
-    }
-
-    /**
      * Checks that this module is configured properly
      *
      * @throws InvalidConfigException
