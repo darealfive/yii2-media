fc16ea24ac9e3d7f00f036b857177d1e79fd3ccd
Sebastian Krein <sebastian@itstrategen.de>
2018-12-01 10:26:19 +0100

    Added view action

---
+++ controllers/actions/View.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * View class file
+ */
+
+namespace darealfive\media\controllers\actions;
+
+/**
+ * Class View
+ *
+ * @package darealfive\media\controllers\actions
+ */
+abstract class View extends Action
+{
+}
---
+++ controllers/actions/image/View.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * View class file
+ */
+
+namespace darealfive\media\controllers\actions\image;
+
+use darealfive\media\controllers\actions\View as ViewAction;
+use darealfive\media\controllers\ImageController;
+
+/**
+ * Class View
+ *
+ * @package darealfive\media\controllers\actions\image
+ *
+ * @property ImageController $controller
+ */
+class View extends ViewAction
+{
+
+}
